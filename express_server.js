d42265fb075ff8efcea6397a6bdf797879d436d4
Jeffery <parkjon8@gmail.com>
2021-12-06 22:58:48 +0000

    Set ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -2,18 +2,33 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
 
 app.get('/', (req, res) => {
-  res.send("Hello");
+  res.send("Hello\n");
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-})
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 // Listenr
 app.listen(PORT, () => {
