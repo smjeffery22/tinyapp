1a748a187848734945bf98f276ccd317cca6fe73
Jeffery <parkjon8@gmail.com>
2021-12-07 02:10:08 +0000

    Added /urls and /urls/:shortURL routes

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -5,13 +6,28 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 }
 
 app.get('/', (req, res) => {
   res.send("Hello\n");
 });
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars); // passing templateVars to the templated called urls.index
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render('urls_show', templateVars);
+});
+
+// /:shortURL => route parameter
+// shortURL is a value stored in req.params object 
+//  if we declare it
+//    req.params.shortURL <=> :shortURL
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
