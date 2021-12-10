ae8f2ed92cd69b71e7c65974c027ac60244b72f2
Jeffery <parkjon8@gmail.com>
2021-12-10 04:07:52 +0000

    Reviewed for submission

--- express_server.js
+++ express_server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 const { checkEmailExistence } = require('./helpers');
 const PORT = 8080;
@@ -13,11 +12,11 @@ app.set('view engine', 'ejs');
 // MIDDLEWARE
 //
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
 }));
+
 
 //
 // DATABASE
@@ -46,11 +45,6 @@ const users = {
   }
 };
 
-// To see users data in .json format
-app.get('/users.json', (req, res) => {
-  res.json(users);
-});
-
 
 //
 // HELPER FUNCTION
@@ -69,6 +63,7 @@ const urlsForUser = (id) => {
       userURL[su] = urlDatabase[su]['longURL'];
     }
   }
+
   return userURL;
 };
 
@@ -93,6 +88,7 @@ app.get('/urls/new', (req, res) => {
     user: users[req.session.user_id]
   };
 
+  // Redirect to login page if a non-user tries to create new URL
   if (!templateVars.user_id) {
     return res.redirect('/login');
   }
@@ -101,20 +97,30 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
+  const allShortUrls = Object.keys(urlDatabase);
+  
+  // Return error if /urls/:?? does not exist
+  if (!allShortUrls.includes(req.params.shortURL)) {
+    res.status(404).send('Page Not Found');
+  }
+
   const templateVars = {
     user_id: req.session.user_id,
     user: users[req.session.user_id],
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]['longURL'] // longURL undefined at first; TinyURL for: on the webpage will be blank
+    longURL: urlDatabase[req.params.shortURL]['longURL']
   };
 
+  // Return error if a non-user tries to access /urls/:shortURL
   if (!templateVars.user_id) {
     return res.send('Non-user prohibited!');
   }
 
+  // Return error if a user who did not create the shortURL tries to access /urls/:shortURL
   if (templateVars.user_id !== urlDatabase[templateVars.shortURL]['userID']) {
     return res.send('You are not allowed here!');
   }
+
 
   res.render('urls_show', templateVars);
 });
@@ -135,7 +141,6 @@ app.get("/u/:shortURL", (req, res) => {
 //
 // REGISTRATION
 //
-// registration page
 app.get('/register', (req, res) => {
   const templateVars = {
     user_id: req.session.user_id,
@@ -164,14 +169,13 @@ app.post('/register', (req, res) => {
     return res.status(400).send('Email already exists!');
   }
 
-  // add user info to the database
+  // add new user info to the database
   users[newId] = {
     id: newId,
     email: newEmail,
     password: hashedPassword
   };
 
-  // res.cookie('user_id', newId);
   req.session['user_id'] = newId;
   
   res.redirect('/urls');
@@ -185,12 +189,13 @@ app.post('/urls', (req, res) => {
   
   const generatedRandomString = generateRandomString();
 
+  // add newly created URL info to the database
   urlDatabase[generatedRandomString] = {
     longURL: req.body.longURL,
     userID: req.session.user_id
   };
   
-  res.redirect(`/urls/${generatedRandomString}`); // redirects to app.get('/urls/:shortURL',
+  res.redirect(`/urls/${generatedRandomString}`);
 });
 
 // delete URL
@@ -203,8 +208,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
     return res.status(403).send('You are not signed in!\n');
   }
 
-  // only owner of /urls/:shortURL can delete
-  //  check if userId matches userID from the urlDatabase
+  // only the user who created URL can delete
   if (userId === urlDatabase[deletedURL]['userID']) {
     delete urlDatabase[deletedURL];
     res.redirect('/urls');
@@ -218,17 +222,15 @@ app.post('/urls/:id', (req, res) => {
   const newLongURL = req.body.editURL;
   const userId = req.session.user_id;
 
-  // if not logged in => cannot delete
+  // if not logged in => cannot edit
   if (!userId) {
     return res.status(403).send('You are not signed in!\n');
   }
 
-  // if (userId === urlDatabase[id]['userID']) {
   urlDatabase[id]['longURL'] = newLongURL;
+  
   res.redirect('/urls');
-  // } else {
-  //   res.send('You are not authorized.');
-  // }
+
 });
 
 //
@@ -257,36 +259,27 @@ app.post('/login', (req, res) => {
   if (!userInfo) {
     return res.status(403).send('Email is not registered! Please register first.');
   }
-    // email registered & password doesn't match => response with 403 status code
+
+  // email registered & password doesn't match => response with 403 status code
   if (bcrypt.compareSync(enteredPassword, userInfo.password) === false) {
     return res.status(403).send('Incorrect password!');
   }
   
-  // upon successful login, set the cookie so the webpage knows the user is logged in
-  // res.cookie('user_id', userInfo.id);
   req.session['user_id'] = userInfo.id;
 
   res.redirect('/urls');
 });
 
+
 //
 // LOGOUT
 //
 app.post('/logout', (req, res) => {
-  // res.clearCookie('user_id');
   req.session = null;
 
   res.redirect('/urls');
 });
 
-
-// app.get('/urls.json', (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n');
-// });
 
 //
 // LISTENER
@@ -294,3 +287,17 @@ app.post('/logout', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+//
+// JSON
+//
+// To see url data in .json format
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// To see users data in .json format
+// app.get('/users.json', (req, res) => {
+//   res.json(users);
+// });
